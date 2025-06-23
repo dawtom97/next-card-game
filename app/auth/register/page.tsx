e68e8a37c54187b54c7c067abe97bc76e9078ce2
MajaZa <zadwornamaja87@gmail.com>
2025-06-23 18:20:35 +0200

    Registration and Login form

--- app/auth/register/page.tsx
+++ app/auth/register/page.tsx
@@ -1,13 +1,22 @@
 "use client";
-import { Button } from "@/components/ui/button";
-import React from "react";
+import { RegisterForm } from "@/components/ui/RegisterForm";
+import { GalleryVerticalEnd } from "lucide-react";
 
 const RegisterPage = () => {
+
+
   return (
     <div>
-      <Button onClick={() => alert("Register button clicked!")}>
-        Kliknij mnie
-      </Button>
+      
+      <div className="flex w-full flex-col gap-6 justify-center items-center bg-muted p-6 md:p-10 min-h-screen">
+        <a href="#" className="flex items-center gap-2 self-center font-medium">
+          <div className="bg-primary text-primary-foreground flex size-6 items-center justify-center rounded-md">
+            <GalleryVerticalEnd className="size-4" />
+          </div>
+          Acme Inc.
+        </a>
+        <RegisterForm/>
+      </div>
     </div>
   );
 };
