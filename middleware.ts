46ba0f1b7d1d7310d753ef11078b9661c1b19509
dawtom97 <daw.tomczyk1997@gmail.com>
2025-07-14 11:13:14 +0200

    auth middleware v2

--- middleware.ts
+++ middleware.ts
@@ -1,18 +1,13 @@
 import { NextRequest, NextResponse } from "next/server";
 
 
+export const config = {
+  matcher: ['/((?!api|_next|static|favicon.ico|robots.txt).*)'],
+};
 
 export function middleware(request: NextRequest) {
   const userId = request.cookies.get("userId");
   const {pathname} = request.nextUrl;
-  if (
-    pathname.startsWith('/_next') ||
-    pathname.startsWith('/api') ||   
-    pathname.startsWith('/favicon.ico') || 
-    pathname.startsWith('/robots.txt') 
-  ) {
-    return NextResponse.next();
-  }
 
   if (!userId && !pathname.includes("/auth")) {
     return NextResponse.redirect(new URL("/auth/login", request.url));
